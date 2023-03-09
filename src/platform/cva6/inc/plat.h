872c0be040026451d8fdf98779fc15b5cfeedf04
D3boker1 <fmarques_00@protonmail.com>
2023-03-09 02:28:23 +0000

    add cva6 plat

---
+++ src/platform/cva6/inc/plat.h
@@ -0,0 +1,10 @@
+#ifndef PLAT_H
+#define PLAT_H
+
+#define PLAT_MEM_BASE 0x80200000
+#define PLAT_MEM_SIZE 0x4000000
+
+#define PLAT_TIMER_FREQ (25000000ull)
+#define UART_IRQ_ID (10) // eu alterei a posicao da intp da uart
+
+#endif
