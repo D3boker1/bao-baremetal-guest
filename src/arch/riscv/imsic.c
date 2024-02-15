b97b94220edb33ac87dfde8d595a71f4b07bd134
D3boker1 <fmarques_00@protonmail.com>
2024-02-15 16:10:24 +0000

    feat(aia): add imsic driver
    Signed-off-by: D3boker1 <fmarques_00@protonmail.com>

---
+++ src/arch/riscv/imsic.c
@@ -0,0 +1,48 @@
+#include <imsic.h>
+#include <cpu.h>
+#include <irq.h>
+
+#define EEID 16
+
+#define CSR_SISELECT			0x150
+#define CSR_SIREG			    0x151
+#define CSR_STOPEI			    0x15c
+
+#define IMSIC_EIDELIVERY		0x70
+#define IMSIC_EITHRESHOLD		0x72
+#define IMSIC_EIP		        0x80
+#define IMSIC_EIE		        0xC0
+
+volatile struct imsic_global* imsic = (void*) IMSICS_BASE;
+
+
+void imsic_init(void){
+    uint32_t prev_val;
+    uint32_t hold;
+
+    /** Enable interrupt delivery */
+    CSRW(CSR_SISELECT, IMSIC_EIDELIVERY);
+    CSRW(CSR_SIREG, 1);
+
+    /** Every intp is triggrable */
+    CSRW(CSR_SISELECT, IMSIC_EITHRESHOLD);
+    CSRW(CSR_SIREG, 0);
+
+    /** Enable all interrupts */
+    CSRW(CSR_SISELECT, IMSIC_EIE);
+    CSRW(CSR_SIREG, 0xffffffffffffffff);
+}
+
+void imsic_send_msi(unsigned long target_cpu_mask, uint32_t data){
+    imsic[target_cpu_mask].s_file.seteipnum_le = data;
+}
+
+void imsic_handle(void){
+    uint32_t intp_identity;
+
+    if ((intp_identity = (CSRR(CSR_STOPEI) >> EEID)) != 0){
+        CSRW(CSR_STOPEI, 0);
+        irq_handle(intp_identity);
+    };
+    
+}
---
+++ src/arch/riscv/inc/imsic.h
@@ -0,0 +1,29 @@
+#ifndef _IMSIC_H_
+#define _IMSIC_H_
+
+#include <core.h>
+#include <csrs.h>
+
+#define IMSICS_BASE 0x28000000
+#define IMSIC_NUM_FILES 1
+
+struct imsic_intp_file_hw
+{
+    uint32_t seteipnum_le;
+    uint32_t seteipnum_be;
+}__attribute__((__packed__, aligned(0x1000ULL)));
+
+struct imsic_global {
+    struct imsic_intp_file_hw s_file;
+} __attribute__((__packed__, aligned(0x1000ULL)));
+
+extern volatile struct imsic_global *imsic;
+
+#define NR_INTP 1024
+
+void imsic_init(void);
+void imsic_handle(void);
+void imsic_send_msi(unsigned long target_cpu_mask, uint32_t data);
+
+
+#endif //_IMSIC_H_
